85a63550b3a84462adc15cd5db68b3720016fa61
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-09-12 07:53:57 -0600

    finished api folder

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,14 +51,59 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(TagData => res.json(TagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(TagData => {
+    if (!TagData) {
+      res.status(404).json({ message: 'No Tag found with this id.'});
+      return;
+    }
+    res.json(TagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(TagData => {
+    if (!TagData) {
+        res.status(404).json({ message: 'No Tag found with this id' });
+        return;
+    }
+    res.json(TagData);
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
